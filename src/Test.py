7a471d5b1060d9c81528ff33dfca15d942a2f85a
Rocky0412 <rockytapader4g@gmail.com>
2025-12-28 18:03:52 +0530

    Added model registry

--- src/Test.py
+++ src/Test.py
@@ -13,7 +13,7 @@ from mlflow.tracking import MlflowClient
 
 client = MlflowClient()
 
-run_id = "e40bbcf3c7184e21b51ad3c144d8954d"
+run_id = "ad20482f393a4e3a92e9f6a3ccf18a7b"
 
 for item in client.list_artifacts(run_id):
     print(item.path)
--- src/register_model.py
+++ src/register_model.py
@@ -1,5 +1,7 @@
 import mlflow
-path='models/m-e3cc9317d5024c65be38aad01111263a/artifacts/model.pkl'
-run_id='e40bbcf3c7184e21b51ad3c144d8954d'
-model_uri=f'runs:/{run_id}/model'
-mlflow.register_model(model_uri=model_uri, name="DecisionTree_final")
+run_id='ad20482f393a4e3a92e9f6a3ccf18a7b'
+result = mlflow.register_model(
+    model_uri=f"runs:/{run_id}/DT_model",
+    name="DecisionTreeClassifier"
+)
+
